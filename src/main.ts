e49bddca58f3deb68dbd8dd93652629abd98347d
tanechai <tiuseko0120@gmail.com>
2020-07-03 19:51:31 +0900

    Apply sort

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { FilesystemTokenStorage } from './services/token-storage/filesystem';
 import { Authorizer } from './services/authorizer';
 import { ChromiumResolver } from './services/chromium/resolver';
 import { ChromiumFinder } from './services/chromium/finder';
+import { SortAssignments } from './sort';
+import { createSortAssignments } from './sort';
 
 const baseUrl = 'https://assignments.onenote.com/api/v1.0';
 
@@ -16,47 +18,82 @@ const baseUrl = 'https://assignments.onenote.com/api/v1.0';
   const tokenProvider = new SavedTokenProvider(tokenStorage, authorizer);
   const proxy = new ApiProxy(baseUrl, tokenProvider);
   const client = new ApiClient(proxy);
+  const assignments = await createSortAssignments(client);
   const readlineSync = require('readline-sync');
-  const modes = ['Assigned','Completed','All'];
-  let index = readlineSync.keyInSelect(modes,'Select mode.');
+  
+  const statusModes = ['Assigned','Completed','All'];
+  let selectedStatus = readlineSync.keyInSelect(statusModes,'Please select the status of assignments.');
+  const sortModes = ['Time','Class'];
+  let selectedSort = readlineSync.keyInSelect(sortModes,'Please select a sorting method.');
+  
+  switch(sortModes[selectedSort]){
+    case 'Time':
+      assignments.sortCheck();
+      assignments.sortClass();
+      assignments.sortTime();
+      break;
 
-  for (const c of await client.getClasses()) {
-    let hasAssignments: boolean = false;
-    for (const a of await client.getAssignments(c.id)) {
-      switch(modes[index]){
-        case 'Assigned':
-          if(!a['isCompleted']){
-            if(!hasAssignments){
-              hasAssignments = !hasAssignments;
-              console.log(`-`, c.name);
-            }
-            console.log('\t', '❗', a.dueDateTime, a.displayName);
-          }
+    case 'Class':
+      assignments.sortTime();
+      assignments.sortCheck();
+      assignments.sortClass();
+      break;
+
+    default:
+      break;
+  }
+  
+  let classId:string;
+  let className:string;
+  
+  for (const a of assignments.getAssignments()){
+    if(classId !=a.classId){
+      for (const c of await client.getClasses()) {
+        if(a.classId == c.id){
+          className = c.name;
           break;
-        case 'Completed':
-          if(a['isCompleted']){
-            if(!hasAssignments){
-              hasAssignments = !hasAssignments;
-              console.log(`-`, c.name);
-            }
-            console.log('\t', '✔', a.dueDateTime, a.displayName);
-          }
-          break;
-        case 'All':
-          if(a['isCompleted'] || !a['isCompleted']){
-            if(!hasAssignments){
-              hasAssignments = !hasAssignments;
-              console.log(`-`, c.name);
-            }
-            console.log('\t', a.dueDateTime, a['isCompleted'] ? '✔' : '❗', a.displayName);
-          }
-          break;
-        default:
-          break;
+        }
       }
     }
+
+    switch(statusModes[selectedStatus]){
+      case 'Assigned':
+        if(!a['isCompleted']){
+          if(className){
+            console.log(`-`, className);
+            className = null;
+          }
+          console.log('\t', '❗', a.dueDateTime, a.displayName);
+        }
+        break;
+
+      case 'Completed':
+        if(a['isCompleted']){
+          if(className){
+            console.log(`-`, className);
+            className = null;
+          }
+          console.log('\t', '✔ ', a.dueDateTime, a.displayName);
+        }
+        break;
+
+      case 'All':
+        if(a){
+          if(className){
+            console.log(`-`, className);
+            className = null;
+          }
+          console.log('\t', a['isCompleted'] ? '✔ ' : '❗', a.dueDateTime, a.displayName);
+        }
+        break;
+
+      default:
+        break;
+    }
+    classId = a.classId;
   }
-  var keyHandler = readlineSync.question('Press any key.');
+
+  var keyHandler = readlineSync.question('Press Enter key.');
 })().catch((error) => {
   console.error(error);
   process.exit(1);
