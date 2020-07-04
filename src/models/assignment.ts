d2e4f1507f4beac0c615bd9caaf254aaccc5301e
Naoki Ikeguchi <root@siketyan.dev>
2020-07-04 12:32:18 +0900

    Add dueDateTime to Assignment model

--- src/models/assignment.ts
+++ src/models/assignment.ts
@@ -1,10 +1,12 @@
 import { ClassId } from './class';
 
 export type AssignmentId = string;
+export type DateTime = string;
 
 export interface Assignment {
   id: AssignmentId;
   classId: ClassId;
+  dueDateTime: DateTime;
   displayName: string;
   isCompleted: boolean;
 }
